3f12173441477e6eb8448a5787bcd25b18b2dc66
Samia Belhaddad <samiamagbelhaddad@gmail.com>
2023-11-21 19:06:10 +0100

    Deploy

--- README.md
+++ README.md
@@ -19,11 +19,43 @@ You can try the app using the Streamlit view from [https://premiumenrollement.st
 [![Streamlit](https://img.shields.io/badge/Streamlit-0.89%2B-green?style=flat&logo=streamlit)](https://streamlit.io/)
 
 
+## Project Methodology:
+#### From Problem to Approach: Asking the right questions
+
+- Business Understanding:  What is the Business Objective ?
+     * The objective is to **predict** whether a costumer will enroll to the app's premius service or not **based on their user data**
+- Use Case :
+    * We want to target the users who are less likely to enroll to make them enroll through campaigns .
+    
+- Understanding our ML Pipeline
+       * Input : User Data from app_data database
+       * output : Enrollment values ( 0 or 1)
+- Framing the Problem :
+     * The problem here is a typical **supervised learning task** since we have Labeled Training examples
+- Designing the System
+     * The Value of the output is 0 or 1 So the goal is **to predict a binary value** .
+     * This is a **regression task** since we are asked to **PREDICT A VALUE**
+     * It is a **Univariate Regression problem** since we Have **one single dependent value to predict** which is "Enrolled"
+- Selecting a performance measure
+     * We are going to test the follwing : ROC Curve (AUC), classification Report and Confusion Matrix.
+
+
+
+## A few Definitions
+* **Machine Learning Pipeline**  :
+  A pipeline is a sequence of data processing components .
+Components typically run **asynchronously** which means that Each component pulls in a large amount
+of data, processes it, and spits out the result in another data storage. Then, some time later (not instantly), the next component in the pipeline pulls this data and spits out its own output .
+* **Logistic Regression** : (also called Logit Regression) is commonly used to estimate the probability that an instance belongs to a particular class . If the estimated probability is greater than 50%, then the model predicts that the instance belongs to that class (called the positive class, labeled “1”), and otherwise it predicts that it does not (i.e., it belongs to the negative class, labeled “0”). This makes it a binary classifier.
+* **Test Set** :  A test set is used to estimate the generalization error that a model will make on new instances, before the model is launched in production
+
+
+
 ### Project Components
 
 - Analyzed and preprocessed data to ensure data quality and integrity.
 - Built a Machine Learning Model to predict the probability of a user purchasing the premium offer.
-- Evaluated and deployed the model as a web app.
+- Evaluated and deployed the model as a streamlit web app.
 
 ### Tools and Technology
 
@@ -31,23 +63,38 @@ You can try the app using the Streamlit view from [https://premiumenrollement.st
 - Streamlit Web App
 - Jupyter Notebook, Visual Studio Code, Git, GitHub
 
-## Metrics and Results
+
+### Metrics and Results
 
 The project evaluation metrics include Confusion Matrix, Precision and Recall, and ROC AUC Curves. Here are some of the results:
+<h3 align="center">Classification Metrics</h3>
+<p align="center">
 
 | Metric          | Score               |
 |-----------------|---------------------|
 | Precision Score | 0.9128              |
 | Recall Score    | 0.8239              |
 | F1 Score        | 0.8661              |
+</p>
 
 
 
-#### Dependencies:
+<h3 align="center">Confusion Matrix</h3>
+<p align="center">
+  <img src="plots/cm.png" alt="Dataset Train & Validation" style="width:50%; height:auto;">
+</p>
+
+
+<h3 align="center">AUC ROC Curves</h3>
+<p align="center">
+  <img src="plots/AUC.png" alt="Dataset Train & Validation" style="width:50%; height:auto;">
+</p>
+
+### Dependencies:
 - Python 3.10.11
 - streamlit 
 
-#### Instructions to Try the Project:
+### Instructions to Try the Project:
 
 * Clone Repo:
 ~~~ 
---
+++ streamlit/app.py
@@ -0,0 +1,149 @@
+# Import necessary libraries
+import streamlit as st
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import joblib
+import tempfile
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from base64 import b64encode
+# Title
+import streamlit as st
+import time
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+st.set_page_config(layout='centered')
+model_path = "saved_models/enrollment_classifier.pkl"
+sample_data = pd.read_csv('sample_data.csv')
+
+
+
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+
+
+def click_button():
+    st.session_state.clicked = True
+
+
+
+
+def make_predictions(data):
+    
+    st.write(data)
+  
+    # StandardScaler for scaling input data
+    scaler = StandardScaler()
+    preprocessed_data = pd.DataFrame(scaler.fit_transform(data),columns=data.columns)
+    
+    
+    # Make predictions using the pre-trained model directly on the uploaded test data
+    predictions = loaded_model.predict(preprocessed_data)  # Assuming your model can predict on the entire DataFrame
+    
+    # Add the predictions as a new column to the test data
+    data['Predicted'] = predictions
+
+
+    # Display the combined DataFrame with predictions
+    st.subheader('Test Data with Predictions')
+    st.write(data)
+    return data
+
+def display_eda(data):
+
+    st.subheader('Exploratory Data Analysis')
+    st.write('1. Data Features')
+    st.write(data.columns)
+    st.write('2. Data shape')
+    st.write(data.shape)
+ 
+    st.write('3. Data head')
+    st.write(data.head())
+    st.write('4. Summary Statistics')
+    st.write(data.describe())
+    st.write('5. Correlation matrix')
+    st.write(data.corr())
+
+    
+
+    return None
+
+
+
+# load the model from disk
+loaded_model = joblib.load(model_path)
+# Centered text
+st.markdown("<h3 style='text-align: center;'>Customer Premium Purchase: Machine Learning</h3>", unsafe_allow_html=True)
+
+st.markdown("<h4 style='text-align: center;'>Get inference on whether users will purchase the premium offer using a pre-trained model</h4>", unsafe_allow_html=True)
+
+st.markdown("<h6 style='text-align: center;'>In working environemnt:</h6>", unsafe_allow_html=True)
+
+st.markdown("<p style='text-align: center;'>to get insights about new users and whether they will purchase the premium offer of the app in the future, the marketing individual would upload a dataset of information about the user behavior, and get the predictions on whether or not they will enroll. </p>",
+             unsafe_allow_html=True)
+st.markdown("<h6 style='text-align: center;'>For the purpose of this demo:</h6>", unsafe_allow_html=True)
+
+st.markdown("<p style='text-align: center;'>A sample dataset with user behavior features is provided by clicking the use sample dataset button , the rest flows.</p>",
+             unsafe_allow_html=True)
+
+st.markdown("<h6 style='text-align: center;'>How to use:</h6>", unsafe_allow_html=True)
+
+
+st.markdown("<p style='text-align: center;'>1. Upload (or use sample) of the user behavior data in csv file<br> 2. A pretrained Random Forest Classifier model with 91% precision is loaded <br> 3. After processing in the backend, the predictions are made <br> 4. You will be able to view the file containing the data you input and predictions in the last column.<br> 5. You can then download the file for further use.</p>",
+             unsafe_allow_html=True)
+
+# Upload CSV file
+uploaded_file = st.file_uploader("Upload a CSV file to get insights on enrollment", type=["csv"])
+# Centered button
+# Create three columns with specified widths
+col1, col2, col3 = st.columns((2.2 , 3, 1))
+
+# Calculate the padding for the center column to center its content
+
+with col2:
+  
+    st.button("Using Upload Dataset",on_click=click_button)
+    st.button("Using Sample Dataset",on_click=click_button)
+# Create three columns with specified widths
+col1, col2, col3 = st.columns((1.5 , 3, 1))
+
+# Calculate the padding for the center column to center its content
+
+with col2:
+    if st.session_state.clicked and uploaded_file is not None:
+        st.write('You selected your data')
+
+       
+        # Read the test dataset
+        data = pd.read_csv(uploaded_file)
+        display_eda(data)
+        predictions = make_predictions(data)
+
+        download_button = st.download_button(
+            label="Download Predictions CSV",
+            data=data.to_csv(index=False).encode('utf-8'),
+            key="download_button",
+            file_name="predictions.csv",
+            mime="text/csv",
+            
+        )
+
+
+    elif st.session_state.clicked:
+
+        data = sample_data
+        
+        display_eda(data)
+        predictions = make_predictions(data)
+            
+    # Add a download button for the DataFrame
+        download_button = st.download_button(
+            label="Download Predictions CSV",
+            data=data.to_csv(index=False).encode('utf-8'),
+            key="download_button",
+            file_name="predictions.csv",
+            mime="text/csv",
+            
+        )
